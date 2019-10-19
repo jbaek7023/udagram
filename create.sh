b9564b522209945c185cacba0ef9450fabee3ff9
John Baek <jbaek@salesforce.com>
2019-10-19 16:11:35 -0700

    Add VPC

--- create.sh
+++ create.sh
@@ -3,3 +3,9 @@ aws cloudformation create-stack \
 --template-body file://$2 \
 --parameters file://$3 \
 --region=us-west-2
+
+# what triggeres cloudformaiotn.yml?
+# aws cloudformation create-stack
+# --stack-name MyStack
+#  --template-body file://MyCloudformationScript.yml
+# --parameters file://MyEnvironmentVariables.json 
